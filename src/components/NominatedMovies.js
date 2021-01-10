7eb978a23dd4be0b77b84eb04813cf2ad75a27d4
Aarthi Ganesh <aarthi.g@live.com>
2021-01-10 01:04:00 -0500

    movie poster sizes fixed

--- src/components/NominatedMovies.js
+++ src/components/NominatedMovies.js
@@ -6,7 +6,13 @@ const NominatedMovies = (props) => {
 		<>
 			{props.movies.map((movie,index) => (
 				<div className="movie">
-					<img src = {`http://img.omdbapi.com/?i=${movie.imdbID}&h=400&apikey=dd6d8910`} alt={movie.Title} className="movie-poster"></img>
+					<div className="movie-poster-crop">
+					<img 
+						src = {`http://img.omdbapi.com/?i=${movie.imdbID}&h=400&apikey=dd6d8910`} 
+						alt={movie.Title} 
+						className="movie-poster"
+						/>
+					</div>
 					<div className="movie-desc">
 						<h4 class="movie-title"> 
 							{movie.Title} 
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -28,11 +28,13 @@ const SearchResults = (props) => {
 		<>
 			{props.movies.map((movie,index) => (
 				<div className="movie">
-					<img src = {`http://img.omdbapi.com/?i=${movie.imdbID}&h=400&apikey=dd6d8910`} 
-					alt={movie.Title} 
-					className="movie-poster"
-					// onError = {this.src=`img/fill.png`}
-					/>
+					<div className="movie-poster-crop">
+						<img 
+						src = {`http://img.omdbapi.com/?i=${movie.imdbID}&h=400&apikey=dd6d8910`} 
+						alt={movie.Title} 
+						className="movie-poster"
+						/>
+					</div>
 					<div className="movie-desc">
 						<h4 class="movie-title"> 
 							{movie.Title} 
